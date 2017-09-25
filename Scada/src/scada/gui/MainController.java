bc3316bb83f753392c81e35881407608752f7e2c
agger1995 <christian_agger1995@hotmail.com>
2017-09-25 09:48:11 +0200

    Repository creation. Added gitignore.

---
+++ Scada/src/scada/gui/MainController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scada.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author chris
+ */
+public class MainController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
