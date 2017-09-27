bf71d14bf45b5bca474600563e3ccdd3bd5bfb77
agger1995 <christian_agger1995@hotmail.com>
2017-09-27 09:16:01 +0200

    Added interfaces.

---
+++ Scada/src/scada/persistence/DatabaseHandler.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scada.persistence;
+
+import scada.domain.IScadaDatabaseFacade;
+
+/**
+ *
+ * @author chris
+ */
+public class DatabaseHandler implements IScadaDatabaseFacade{
+    
+}
