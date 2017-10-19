4a652f938d04631ecd6bd33f40fe42794b16a6d6
Mads Thy <mads@heimdal-thy.dk>
2017-10-19 17:35:23 +0200

    #MATHY TestGreenhouse can now send messages to a Virtual Greenhouse

--- Scada/src/PLCCommunication/TestGreenhouse.java
+++ Scada/src/PLCCommunication/TestGreenhouse.java
@@ -12,15 +12,14 @@ import API.IGreenhouse;
  * API tester
  * @author sps
  */
-public class TestGreenhouse 
-{
-    public static void main(String[] args) 
+public class TestGreenhouse {
+    public static void main(String[] args)
     {
         PLCConnection con = new UDPConnection(1025, "localhost");
         //PLCConnection con = new UDPConnection(5000, "192.168.0.40");
         //PLCConnection con = new SerialConnection("COM4");
         //SerialConnection.getPortList("COM1");
-        
+
         IGreenhouse api = new Greenhouse(con);
 //        while(true){
 //            int i = 0;
@@ -33,24 +32,24 @@ public class TestGreenhouse
 //                api.SetBlueLight(i);
 //            }
 //        }
-//        api.SetRedLight(50);
-//        api.SetBlueLight(100);
-//        api.SetTemperature(273 + 25);
-//        api.SetFanSpeed(2);
+        api.SetRedLight(50);
+        api.SetBlueLight(100);
+        api.SetTemperature(273 + 25);
+        api.SetFanSpeed(2);
         api.SetBlueLight(100);                      // bit 4
         api.AddWater(3);                            // bit 6
         api.ReadTemp2();                            // bit 10
-        api.SetFanSpeed(1);                         // bit 16
         api.SetTemperature(300);                    // bit 1
         double outdoorTemperature;
-        while (true) {
-            byte[] status = api.GetStatus();
-            outdoorTemperature = api.ReadTemp1();    // bit 9
+        //while (true)
+        api.SetFanSpeed(1);
+//           outdoorTemperature = api.ReadTemp1();    // bit 9
 
-        }
+
+
 
 
         //System.exit(3);
     }
-    
+
 }
