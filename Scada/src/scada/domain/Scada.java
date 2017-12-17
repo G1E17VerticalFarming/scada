8cdc3623bb2cdc681cbb0a34e6ca063479cbbdc4
MadsLunde <mads.lunde31@gmail.com>
2017-12-17 16:35:16 +0100

    Revert "Fixed a single bug i SCADA, and updated POM to handle 1.5 FLIB."
    This reverts commit 9e18100877a56d2287e48e5da233737cf411c00f.

--- Scada/src/scada/domain/Scada.java
+++ Scada/src/scada/domain/Scada.java
@@ -108,7 +108,7 @@ public class Scada implements IScada {
     private ArrayList<ProductionBlock> readPLCList() {
         try {
             return (ArrayList<ProductionBlock>) readWriteProductionBlock.readPLCFile();
-        } catch (IOException ex) {
+        } catch (IOException | ClassNotFoundException ex) {
             System.out.println(ex);
             return new ArrayList<>();
         }
@@ -121,7 +121,7 @@ public class Scada implements IScada {
     private ArrayList<GrowthProfile> readGbList() {
         try {
             return (ArrayList<GrowthProfile>) this.readWriteGrowthProfile.readGrowthProfileFile();
-        } catch (IOException ex) {
+        } catch (IOException | ClassNotFoundException ex) {
             System.out.println(ex);
             return new ArrayList<>();
         }
